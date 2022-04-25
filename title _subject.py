723b5bafed5f8c5f5d110e8bec7e7fb06cc11250
ClaudiusMa <35928249+ClaudiusMa@users.noreply.github.com>
2022-04-24 20:37:07 -0400

    Add files via upload

---
+++ title _subject.py
@@ -0,0 +1,45 @@
+import json
+import unittest
+import os
+import requests
+
+
+
+
+def read_list(lst):
+    dic = {}
+    for item in lst:
+        topic = request(item)
+        if topic in dic.keys():
+            dic[topic].append(item)
+
+        else:
+            dic[topic]=[item]
+    print(dic)
+    return dic
+
+
+def find_type(api_return):
+
+    type_list = api_return["results"][0]["subjects"]
+    for item in type_list:
+        if "--" in item:
+            lst = item.split(" -- ")
+            type = lst[1]
+            return type
+
+
+
+
+def request(bookname):
+    bookname.lower()
+    base_url = "https://gutendex.com/books/?"
+    para = "search=" + str(bookname)
+    url = base_url + para
+    r = requests.get(url)
+    dict = json.loads(r.text)
+    type = find_type(dict)
+    return type
+
+lst = ["Great Expectations", "Deathworld","Harry's Ladder to Learning"]
+read_list(lst)
